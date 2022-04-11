4832b81908392006e784d1408b29c14cc133368c
Andy Whiteside <andywhiteside@gmail.com>
2022-04-10 22:45:51 -0400

    alpha2

--- src/lib/Path.svelte
+++ src/lib/Path.svelte
@@ -2,10 +2,10 @@
   import Button from './Button.svelte'
 </script>
 
-<div class="flex justify-between">
-  <Button name="SQUAT" />
-  <Button name="PRESS" />
-  <Button name="BENCH PRESS" />
-  <Button name="DEADLIFT" />
-  <Button name="POWER CLEAN" />
+<div class="flex justify-around">
+  <Button name="SQ" />
+  <Button name="PR" />
+  <Button name="BP" />
+  <Button name="DL" />
+  <Button name="PC" />
 </div>
