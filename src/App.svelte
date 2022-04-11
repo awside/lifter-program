f99df473b31b0c55bebab558160fc21c31aed0e8
Andy Whiteside <andywhiteside@gmail.com>
2022-04-10 22:40:43 -0400

    alpha1

--- src/App.svelte
+++ src/App.svelte
@@ -2,12 +2,16 @@
   import Barbell from './lib/Barbell.svelte'
   import Header from './lib/Header.svelte'
   import LiftNumbers from './lib/LiftNumbers.svelte'
+import Path from './lib/Path.svelte';
+import Workout from './lib/Workout.svelte';
 </script>
 
 <main>
   <Header />
   <div class="container max-w-lg mx-auto space-y-5 py-5 px-5">
     <Barbell />
+    <Path />
+    <Workout />
     <LiftNumbers />
   </div>
 </main>
---
+++ src/lib/Button.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  export let name: string
+
+  const handleClick = () => {
+    console.log(name)
+  }
+</script>
+
+<div
+  on:click={handleClick}
+  class="border-2 border-blue-500 px-2 py-1 rounded-md text-blue-500 text-sm"
+>
+  {name}
+</div>
